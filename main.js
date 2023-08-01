33ec260b14ffce3f4bae5c9a470eca28a59100ba
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-01 15:05:22 +0330

    add box--tasks

--- main.js
+++ main.js
@@ -10,7 +10,22 @@ const showTodaysTaskCount = () => {
   } tasks`;
 };
 
+const showTaksSummary = () => {
+  const tasksDiv = document.getElementById("box--tasks");
+  const tasks = filterTodaysTasks();
+  tasksDiv.innerHTML = "";
+  tasksDiv.innerHTML += `<div class="task-card">
+    <div class="badge">${tasks.length}</div>
+    <span>tasks created</span>
+    </div>`;
+  tasksDiv.innerHTML += `<div class="task-card">
+    <div class="badge">${tasks.filter((t) => !t.done).length}</div>
+    <span>tasks left</span>
+    </div>`;
+};
+
 const documentReady = () => {
   showTodaysTaskCount();
+  showTaksSummary();
 };
 document.addEventListener("DOMContentLoaded", documentReady);
